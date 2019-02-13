9591730001c7c5e0b73a0ea2d76b58989aee7c8a
amaan19 <amaan.ahmed3@gmail.com>
2019-02-13 07:27:13 +0000

    Basic field layout, added functions to create starting hand, draw a card, summon monster

---
+++ src/index.js
@@ -0,0 +1,267 @@
+deck1 = [
+    {
+        "id": "549481",
+        "name": "Prevent Rat",
+        "desc": "This creature is shielded with a tough hide of hair and is excellent at defending itself.",
+        "atk": "500",
+        "def": "2000",
+        "type": "Normal Monster",
+        "level": "4",
+        "race": "Beast",
+        "attribute": "EARTH",
+        "image_url": "https://storage.googleapis.com/ygoprodeck.com/pics/549481.jpg",
+        "image_url_small": "https://storage.googleapis.com/ygoprodeck.com/pics_small/549481.jpg"
+    },
+    {
+        "id": "732302",
+        "name": "Temple of Skulls",
+        "desc": "A mysterious temple of skulls and bones that pulls in unsuspecting enemies.",
+        "atk": "900",
+        "def": "1300",
+        "type": "Normal Monster",
+        "level": "4",
+        "race": "Zombie",
+        "attribute": "DARK",
+        "image_url": "https://storage.googleapis.com/ygoprodeck.com/pics/732302.jpg",
+        "image_url_small": "https://storage.googleapis.com/ygoprodeck.com/pics_small/732302.jpg"
+    },
+    {
+        "name": "Kojikocy",
+        "desc": "A man-hunter with powerful arms that can crush boulders.",
+        "atk": "1500",
+        "def": "1200",
+        "type": "Normal Monster",
+        "level": "4",
+        "race": "Warrior",
+        "attribute": "EARTH",
+        "image_url": "https://storage.googleapis.com/ygoprodeck.com/pics/1184620.jpg",
+        "image_url_small": "https://storage.googleapis.com/ygoprodeck.com/pics_small/1184620.jpg"
+    },
+    {
+        "id": "1784619",
+        "name": "Uraby",
+        "desc": "Fast on its feet, this dinosaur rips enemies to shreds with its sharp claws.",
+        "atk": "1500",
+        "def": "800",
+        "type": "Normal Monster",
+        "level": "4",
+        "race": "Dinosaur",
+        "attribute": "EARTH",
+        "image_url": "https://storage.googleapis.com/ygoprodeck.com/pics/1784619.jpg",
+        "image_url_small": "https://storage.googleapis.com/ygoprodeck.com/pics_small/1784619.jpg"
+    },
+    {
+        "id": "2118022",
+        "name": "Hyosube",
+        "desc": "This amphibian is strong on the attack, but leaves much to be desired when defending.",
+        "atk": "1500",
+        "def": "900",
+        "type": "Normal Monster",
+        "level": "4",
+        "race": "Aqua",
+        "attribute": "WATER",
+        "image_url": "https://storage.googleapis.com/ygoprodeck.com/pics/2118022.jpg",
+        "image_url_small": "https://storage.googleapis.com/ygoprodeck.com/pics_small/2118022.jpg"
+    },
+    {
+        "id": "5053103",
+        "name": "Battle Ox",
+        "desc": "A monster with tremendous power, it destroys enemies with a swing of its axe.",
+        "atk": "1700",
+        "def": "1000",
+        "type": "Normal Monster",
+        "level": "4",
+        "race": "Beast-Warrior",
+        "attribute": "EARTH",
+        "image_url": "https://storage.googleapis.com/ygoprodeck.com/pics/5053103.jpg",
+        "image_url_small": "https://storage.googleapis.com/ygoprodeck.com/pics_small/5053103.jpg"
+    },
+    {
+        "id": "5265750",
+        "name": "Skull Mariner",
+        "desc": "A pirate ship that appears out of the mist and sinks any seagoing vessels.",
+        "atk": "1600",
+        "def": "900",
+        "type": "Normal Monster",
+        "level": "4",
+        "race": "Warrior",
+        "attribute": "WATER",
+        "image_url": "https://storage.googleapis.com/ygoprodeck.com/pics/5265750.jpg",
+        "image_url_small": "https://storage.googleapis.com/ygoprodeck.com/pics_small/5265750.jpg"
+    }
+]
+
+deck2 = [
+    {
+        "id": "2311603",
+        "name": "Overdrive",
+        "desc": "An all-terrain armored vehicle armed with a heavy-duty machine gun.",
+        "atk": "1600",
+        "def": "1500",
+        "type": "Normal Monster",
+        "level": "4",
+        "race": "Machine",
+        "attribute": "EARTH",
+        "image_url": "https://storage.googleapis.com/ygoprodeck.com/pics/2311603.jpg",
+        "image_url_small": "https://storage.googleapis.com/ygoprodeck.com/pics_small/2311603.jpg"
+    },
+    {
+        "id": "2483611",
+        "name": "Water Omotics",
+        "desc": "Transforms the water overflowing from a jar into attacking dragons.",
+        "atk": "1400",
+        "def": "1200",
+        "type": "Normal Monster",
+        "level": "4",
+        "race": "Aqua",
+        "attribute": "WATER",
+        "image_url": "https://storage.googleapis.com/ygoprodeck.com/pics/2483611.jpg",
+        "image_url_small": "https://storage.googleapis.com/ygoprodeck.com/pics_small/2483611.jpg"
+    },
+    {
+        "id": "2863439",
+        "name": "Fiend Reflection 2",
+        "desc": "A bird-beast that summons reinforcements with a hand mirror.",
+        "atk": "1100",
+        "def": "1400",
+        "type": "Normal Monster",
+        "level": "4",
+        "race": "Winged Beast",
+        "attribute": "LIGHT",
+        "image_url": "https://storage.googleapis.com/ygoprodeck.com/pics/2863439.jpg",
+        "image_url_small": "https://storage.googleapis.com/ygoprodeck.com/pics_small/2863439.jpg"
+    },
+    {
+        "id": "2906250",
+        "name": "Grappler",
+        "desc": "A devious snake with a thick body that wraps around an enemy monster and squeezes the life out of it.",
+        "atk": "1300",
+        "def": "1200",
+        "type": "Normal Monster",
+        "level": "4",
+        "race": "Reptile",
+        "attribute": "WATER",
+        "image_url": "https://storage.googleapis.com/ygoprodeck.com/pics/2906250.jpg",
+        "image_url_small": "https://storage.googleapis.com/ygoprodeck.com/pics_small/2906250.jpg"
+    },
+    {
+        "id": "3134241",
+        "name": "Flying Kamakiri 2",
+        "desc": "A flying mantis that feeds primarily on insects.",
+        "atk": "1500",
+        "def": "800",
+        "type": "Normal Monster",
+        "level": "4",
+        "race": "Insect",
+        "attribute": "WIND",
+        "image_url": "https://storage.googleapis.com/ygoprodeck.com/pics/3134241.jpg",
+        "image_url_small": "https://storage.googleapis.com/ygoprodeck.com/pics_small/3134241.jpg"
+    }
+]
+
+state = {
+    p1: {
+        life: null,
+        deck: deck1,
+        hand: [],
+        field: [],
+        currentCard: null
+    },
+    p2: {
+        life: null,
+        deck: deck2,
+        hand: [],
+        field: [],
+        currentCard: null
+    },
+    inplay: null
+}
+
+p1 = state.p1
+p2 = state.p2
+
+
+gameStart = () => {
+    p1.p1life = 4000
+    p2.p2life = 4000
+}
+
+//DISPLAYING CARDS 
+
+//Hand
+var handEl = document.querySelector('#my-hand')
+
+startingHand = () => {
+    for (let i = 0; i < 3; i++) {
+        card = p1.deck.pop()
+        renderHandCard(card)
+        p1.hand.push(card)
+    }
+}
+
+renderHandCard = card => {
+    cardImg = document.createElement('img')
+    cardImg.src = card.image_url
+    cardImg.id = card.id
+    handEl.appendChild(cardImg)
+}
+
+
+
+//Attacking 
+attackVattack = (myMonster, oppMonster) => {
+    difference = myMonster.atk - oppMonster.atk
+    if (difference > 0) {
+        // Write this function
+        // destroyMonster(opp.card)
+        p2.life -= difference
+    } else if (difference < 0) {
+        // destroyMonster(myMonster)
+        p1.life += difference
+    } else {
+    }
+}
+
+//Destroying monster 
+destroyMonster = monster => {
+
+}
+
+//Draw card 
+drawCard = () => {
+    card = p1.deck.pop()
+    p1.hand.push(card)
+    renderHandCard(card)
+}
+
+//Play card 
+const fieldEl = document.querySelector('.my-monster-field')
+
+renderFieldCard = card => {
+    cardImg = document.createElement('img')
+    cardImg.src = card.image_url
+    fieldEl.appendChild(cardImg)
+}
+
+playCard = card => {
+    // debugger
+    renderFieldCard(card)
+    removeCard = document.getElementById(`${card.id}`)
+    handEl.remove(removeCard)
+
+}
+
+const handCards = document.querySelector('.non-field-cards')
+
+handCards.addEventListener('click', e => {
+    car = p1.hand.find(card => card.id == e.target.id)
+    playCard(car)
+})
+
+
+initialize = () => {
+    gameStart()
+    startingHand()
+}
+
+initialize() 
