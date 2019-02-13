4240f2c2ff04f9c5cc0e1cecb4ca29f230340c8b
amaan19 <amaan.ahmed3@gmail.com>
2019-02-13 14:50:52 +0000

    Fixed attacking

--- src/index.js
+++ src/index.js
@@ -26,6 +26,7 @@ deck1 = [
         "image_url_small": "https://storage.googleapis.com/ygoprodeck.com/pics_small/732302.jpg"
     },
     {
+        "id": "1184620",
         "name": "Kojikocy",
         "desc": "A man-hunter with powerful arms that can crush boulders.",
         "atk": "1500",
@@ -174,7 +175,7 @@ state = {
         field: [],
         currentCard: null
     },
-    inplay: null
+    inplay: []
 }
 
 p1 = state.p1
@@ -182,8 +183,8 @@ p2 = state.p2
 
 
 gameStart = () => {
-    p1.p1life = 4000
-    p2.p2life = 4000
+    p1.life = 4000
+    p2.life = 4000
 }
 
 //DISPLAYING CARDS 
@@ -191,11 +192,31 @@ gameStart = () => {
 //Hand
 var handEl = document.querySelector('#my-hand')
 
+//My Helper functions
+
+addToHandState = card => {
+    p1.hand.push(card)
+}
+
+addToFieldState = card => {
+    p1.field.push(card)
+    state.inplay.push(card)
+}
+
+removeFromHand = card => {
+    p1.hand = p1.hand.filter(c => c !== card)
+    renderHandCards(p1.hand)
+}
+
+removeFromFieldState = card => {
+    p1.field = p1.field.filter(c => c !== card)
+}
+
 startingHand = () => {
     for (let i = 0; i < 3; i++) {
         card = p1.deck.pop()
         renderHandCard(card)
-        p1.hand.push(card)
+        addToHandState(card)
     }
 }
 
@@ -206,52 +227,114 @@ renderHandCard = card => {
     handEl.appendChild(cardImg)
 }
 
+renderHandCards = cards => {
+    handCards.innerHTML = ''
+    cards.forEach(card => {
+        renderHandCard(card)
+    })
+}
+
+findCard = id => {
+    return card = p1.hand.find(c => c.id === id)
+}
+
+renderFieldMonsters = cards => {
+    fieldEl.innerHTML = ''
+    cards.forEach(c => renderFieldMonster(c))
+}
+
+//Opponent helper functions 
+removeFromOppFieldState = card => {
+    p2.field = p2.field.filter(c => c !== card)
+}
+
+const oppFieldEl = document.querySelector('.opp-field')
+
+renderOppFieldMonster = card => {
+    cardImg = document.createElement('img')
+    cardImg.src = card.image_url
+    cardImg.id = card.id
+    oppFieldEl.appendChild(cardImg)
+    p2.field.push(card)
+}
+
+renderOppFieldMonsters = cards => {
+    oppFieldEl.innerHTML = ''
+    cards.forEach(c => renderOppFieldMonster(c))
+}
 
 
 //Attacking 
 attackVattack = (myMonster, oppMonster) => {
     difference = myMonster.atk - oppMonster.atk
     if (difference > 0) {
-        // Write this function
-        // destroyMonster(opp.card)
+        destroyOppMonster(oppMonster)
         p2.life -= difference
+        renderOppFieldMonsters(p2.field)
     } else if (difference < 0) {
-        // destroyMonster(myMonster)
+        destroyMyMonster(myMonster)
         p1.life += difference
     } else {
     }
 }
+const fieldEl = document.querySelector('.my-monsters')
+
+fieldEl.addEventListener('click', e => {
+    if (e.target.nodeName === "IMG") {
+        myCard = p1.field.find(c => c.id === e.target.id)
+
+        oppFieldEl.addEventListener('click', ev => {
+            if (ev.target.nodeName === "IMG") {
+                oppCard = p2.field.find(c => c.id === ev.target.id)
+
+                attackVattack(myCard, oppCard)
+            }
+        })
+    }
+
+})
 
 //Destroying monster 
-destroyMonster = monster => {
+destroyMyMonster = card => {
+    removeFromFieldState(card)
+    renderFieldMonsters(p1.field)
+}
 
+destroyOppMonster = card => {
+    removeFromOppFieldState(card)
+    renderOppFieldMonsters(p2.field)
 }
 
 //Draw card 
+const deck = document.querySelector('#my-deck')
+
 drawCard = () => {
     card = p1.deck.pop()
-    p1.hand.push(card)
+    addToHandState(card)
     renderHandCard(card)
 }
+deck.addEventListener('click', () => {
+    drawCard()
+})
 
 //Play card 
-const fieldEl = document.querySelector('.my-monster-field')
 
-renderFieldCard = card => {
+
+renderFieldMonster = card => {
     cardImg = document.createElement('img')
     cardImg.src = card.image_url
+    cardImg.id = card.id
     fieldEl.appendChild(cardImg)
 }
 
 playCard = card => {
-    // debugger
-    renderFieldCard(card)
-    removeCard = document.getElementById(`${card.id}`)
-    handEl.remove(removeCard)
-
+    renderFieldMonster(card)
+    addToFieldState(card)
+    removeFromHand(card)
+    renderHandCards(p1.hand)
 }
 
-const handCards = document.querySelector('.non-field-cards')
+const handCards = document.querySelector('#my-hand')
 
 handCards.addEventListener('click', e => {
     car = p1.hand.find(card => card.id == e.target.id)
